a2aaa0c8752f7db3d79c27ba2676460cbf682979
Jamie Colley <jamie.colley.88@gmail.com>
2013-06-08 07:10:48 -0400

    Page Navigation

---
+++ BlogDemoApp/About.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Microsoft.Phone.Tasks;
+
+namespace BlogDemoApp
+{
+    public partial class About : PhoneApplicationPage
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        public void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        /*
+
+        public void EmailBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var emailer = new EmailComposeTask();
+            emailer.To = "jamie@dotnetgeekette.com";
+            emailer.Subject = "Sample App Inquiry";
+            emailer.Show();
+        }
+
+        public void ReviewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var reviewer = new MarketplaceReviewTask();
+            reviewer.Show();
+        }
+
+        public void OtherAppsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var searcher = new MarketplaceSearchTask();
+            searcher.SearchTerms = "Jerrel Blankenship";
+            searcher.Show();
+        }
+         * 
+         */
+
+    
+    }
+}
--- BlogDemoApp/MainPage.xaml.cs
+++ BlogDemoApp/MainPage.xaml.cs
@@ -22,20 +22,9 @@ namespace BlogDemoApp
             //BuildLocalizedApplicationBar();
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        public void AboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/About.xaml", UriKind.Relative));
+        }
     }
 }
